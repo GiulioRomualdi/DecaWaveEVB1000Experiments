046c8bcb1aa5b124be3242b9147f01ad423a9000
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-08-03 18:08:44 +0200

    add laser measurements of the experiments done on August 2nd

---
+++ test_02_08_2017/laser_data.m
@@ -0,0 +1,9 @@
+function out = laser_data()
+    out = struct();
+    out.('r01').('laser') = 1.983;
+    out.('r02').('laser') = 5.470;
+    out.('r03').('laser') = 3.835;
+    out.('r12').('laser') = 4.393;
+    out.('r13').('laser') = 4.280;
+    out.('r23').('laser') = 3.669;
+end
