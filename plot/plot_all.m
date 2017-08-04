468caf2fd171400c761144af3fdd63e1c4190896
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-08-04 16:06:23 +0200

    add plot_a2a_histograms

--- plot/plot_all.m
+++ plot/plot_all.m
@@ -2,3 +2,4 @@ plot_path(path, autoranging, 'laser', plane_height, 1);
 plot_path(path, autoranging, 'tag', plane_height, 1);
 plot_path(path, autoranging, 'joined', plane_height, 1);
 
+plot_a2a_histograms(autoranging);
