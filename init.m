19102be6b4aad65d07c62283e11f974eec822cad
xEnVrE <nicolapiga@gmail.com>
2017-08-04 11:15:26 +0200

    Add script to add required path to search path of MATLAB

---
+++ init.m
@@ -0,0 +1,8 @@
+% add required path to search path
+
+% data_load_scripts contains scripts required to load data contained in
+% test_dd_mm_year folders
+addpath('data_load_scripts/');
+
+% trilateration contains trilat algorithms
+addpath('trilateration/');
