7d2190fd642fd431631ebe30864cc3aee949fd2f
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-08-10 10:12:33 +0200

    add plane height to experimenet (02/08/17)

--- test_02_08_2017/laser_data.m
+++ test_02_08_2017/laser_data.m
@@ -1,9 +1,12 @@
-function out = laser_data()
-    out = struct();
-    out.('r01').('laser') = 1.983;
-    out.('r02').('laser') = 5.470;
-    out.('r03').('laser') = 3.835;
-    out.('r12').('laser') = 4.393;
-    out.('r13').('laser') = 4.280;
-    out.('r23').('laser') = 3.669;
+function [a2a_manual_meas, plane_height] = laser_data()
+
+    plane_height = 1.653;
+    
+    a2a_manual_meas = struct();
+    a2a_manual_meas.('r01').('laser') = 1.983;
+    a2a_manual_meas.('r02').('laser') = 5.470;
+    a2a_manual_meas.('r03').('laser') = 3.835;
+    a2a_manual_meas.('r12').('laser') = 4.393;
+    a2a_manual_meas.('r13').('laser') = 4.280;
+    a2a_manual_meas.('r23').('laser') = 3.669;
 end
