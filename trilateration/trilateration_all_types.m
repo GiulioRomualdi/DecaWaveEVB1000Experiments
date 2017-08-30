d83dc15ca59920b465cd99bdf830e783ee235890
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-08-30 12:46:25 +0200

    handle the case in which tag over anchor data are not present

--- trilateration/trilateration_all_types.m
+++ trilateration/trilateration_all_types.m
@@ -2,8 +2,13 @@ function trilateration = trilateration_all_types(path, autoranging)
 
     % empty trilateration
     trilateration = struct();
-    type_names = {'joined','tag','laser'};
 
+    if(isfield(autoranging.r01,'tag'))
+        type_names = {'joined','tag','laser'};
+    else
+        type_names = {'joined','laser'};
+    end
+    
     % cycle through type names
     for name = type_names
         trilateration = trilat_by_type(path, autoranging, name{:}, trilateration);
