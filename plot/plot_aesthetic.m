a7cedb16a653e8ced77fc4f9edcfcf94909b2c28
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-10-02 16:04:40 +0200

    -add latex syntax for title and labels. -change textwidth -change linewidth -add grid

--- plot/plot_aesthetic.m
+++ plot/plot_aesthetic.m
@@ -1,18 +1,38 @@
-function [] = plot_aesthetic(Title, Label_x, Label_y, varargin)
+function plot_aesthetic(Title, Label_x, Label_y, Label_z, varargin)
 % PLOT_AESTHETIC add Title, label and legends in a plot
-%   PLOT_AESTHETIC(Title, Label_x, Legend_1, ..., Legend_n) add Title, 
-%   label and legends in a plot. LaTex syntax is allowed in legends
-%   strings.
+%   PLOT_AESTHETIC(Title, Label_x, Label_y, Label_z, Legend_1, ..., Legend_n)
+%   add title, labels and legends in a plot. LaTex syntax is allowed.
 
-title(Title)
-xlabel(Label_x)
-ylabel(Label_y)
+% set labels
+x_label = xlabel(Label_x);
+y_label = ylabel(Label_y);
+z_label = zlabel(Label_z);
+set(x_label,'Interpreter','latex')
+set(y_label,'Interpreter','latex')
+set(z_label,'Interpreter','latex')
+set(x_label,'FontSize', 16);
+set(y_label,'FontSize', 16);
+set(z_label,'FontSize', 16);
 
-h = legend(varargin{:});
-set(h,'Interpreter','latex')
-set(h,'FontSize',12);
-h = findobj(gcf,'type','line');
-set(h,'linewidth',1.2);
-grid on;
+% set legend
+if ~isempty(varargin)
+    h = legend(varargin{:}, 'Location', 'best');
+    set(h,'Interpreter','latex')
+    set(h,'FontSize', 16);
 end
 
+% change linewidth
+h = findobj(gcf,'type','line');
+set(h,'linewidth',1.2)
+
+% set the title
+tit = title(Title);
+set(tit,'FontSize', 25);
+set(tit,'Interpreter','latex');
+
+% change font size
+set(gca,'FontSize', 16)
+
+% set grid
+grid on;
+end
