769cd960d8a92584da1c9287eb94ca15f1c0024a
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-10-04 18:38:00 +0200

    - general refactoring of plot_component function - remove plot_components function

--- plot/plot_component.m
+++ plot/plot_component.m
@@ -1,45 +1,11 @@
-function plot_component(trilateration_data, anchor_pos_data_type, ...
-                               skip_frames, component)
-                     
-    hold on
-    
-    % extract algorithm name from trilateration_data
-    alg_names = fieldnames(trilateration_data.(anchor_pos_data_type))';
-    
-    % cycle over algorithms
-    for alg_name = alg_names
-        
-        % extract trilateration data
-        positions = trilateration_data.(anchor_pos_data_type).(alg_name{:});
-        
-        % cut signals according to skip_frames
-        signal = positions.(component);
-        signal = signal(skip_frames:end);
-        
-        % make plot
-        plot(signal);    
-    end
-        
+function plot_component(signal, skip_frames, Title, component, Legend)
+               
+hold on
+
+% plot the signal
+signal = signal(skip_frames:end);
+plot(signal);
    
-   % print legend
-   legend(alg_names{:})
-   
-   xlabel('samples');
-   ylabel(strcat(component,' (m)'));
-   
-   % print title
-   Title = "Trilateration";
-   
-   if strcmp(anchor_pos_data_type, 'tag')
-        Title = strcat(Title, ' (anchors position with tag over anchors)');
-   elseif strcmp(anchor_pos_data_type, 'laser')
-       Title = strcat(Title, '  (anchors position with manual measurements)');
-   elseif strcmp(anchor_pos_data_type, 'joined')
-       Title = strcat(Title, ' (anchors position with autoranging procedures)');
-   end
-    
-   title(Title);
-   
-   % set grid
-   grid on;
+% set the Title, the labels and the legend
+plot_aesthetic(Title, 'samples', strcat('$',component,'$ (m)'),'', Legend);   
 end
