0f2ad001471129a52595129d2d446b3479ab871f
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-10-04 13:42:20 +0200

    remove useless file

--- trilateration/Untitled.m
+++
@@ -1,9 +0,0 @@
-function [ output_args ] = Untitled( anchor_pos_data )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-% eval anchor positions using anchor_to_anchor data
-    anchor_positions = eval_anch_pos(anchor_pos_data, anchor_pos_data_type);
-
-end
-
