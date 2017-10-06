669765965d0a76cdc5bcda7e135080c82c1d9db7
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-10-06 18:13:40 +0200

    add some comments in print_figure.m and in plot_component.m

--- plot/plot_component.m
+++ plot/plot_component.m
@@ -1,5 +1,13 @@
 function plot_component(signal, skip_frames, Title, component, Legend)
-               
+% PLOT_COMPONENT plot a signal
+%   PLOT_COMPONENT(signal, skip_frames, Title, component, Legend) plot a
+%   signal in a already existing figure
+%   * signal: vector of the signal that will be plotted;
+%   * skip_frames: number of frame that will be skipped in the plot;
+%   * Title: title of the plot;
+%   * skip_frames: string of the component (i.e. 'x', 'y' or 'z');
+%   * Legend: legend associated to the plot.
+
 hold on
 
 % plot the signal
--- plot/print_figure.m
+++ plot/print_figure.m
@@ -1,5 +1,7 @@
 function print_figure(filename)
+% PRINT_FIGURE save a figure in a pdf file
+%   PRINT_FIGURE(filename) save the current figure in a pdf file
 
-    % if a subplot figure want to be save use png extension instead of pdf
-    export_fig(strcat('figures/',filename,'.pdf'), '-transparent');
+% if a subplot figure want to be save use png extension instead of pdf
+export_fig(strcat('figures/',filename,'.pdf'), '-transparent');
 end
