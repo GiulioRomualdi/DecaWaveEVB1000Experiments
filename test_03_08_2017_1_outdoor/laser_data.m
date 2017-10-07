6db51b4f7957e120c5561315857d52fd83416595
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-10-07 15:36:57 +0200

    add laser measurement to the an already existing structure

--- test_03_08_2017_1_outdoor/laser_data.m
+++ test_03_08_2017_1_outdoor/laser_data.m
@@ -1,15 +1,12 @@
-function [a2a_manual_meas, plane_height] = laser_data()
-
-    a2a_manual_meas = struct();
-    
+function [data, plane_height] = laser_data(data)
     plane_height = 1.875;
     anchor3_height = 2.75;
     
     delta_h = anchor3_height - plane_height;
-    a2a_manual_meas.('r01').('laser') = 17.67;
-    a2a_manual_meas.('r02').('laser') = 11.80;
-    a2a_manual_meas.('r03').('laser') = sqrt(delta_h^2 + 9^2);
-    a2a_manual_meas.('r12').('laser') = 16.94;
-    a2a_manual_meas.('r13').('laser') = sqrt(delta_h^2 + 10.83^2);
-    a2a_manual_meas.('r23').('laser') = sqrt(delta_h^2 + 7.17^2);
+    data.('r01').('laser') = 17.67;
+    data.('r02').('laser') = 11.80;
+    data.('r03').('laser') = sqrt(delta_h^2 + 9^2);
+    data.('r12').('laser') = 16.94;
+    data.('r13').('laser') = sqrt(delta_h^2 + 10.83^2);
+    data.('r23').('laser') = sqrt(delta_h^2 + 7.17^2);
 end
