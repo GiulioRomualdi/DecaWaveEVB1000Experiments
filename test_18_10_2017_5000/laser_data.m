ded36c4a30eed29750e7d5825b5774d7c388215f
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-10-18 15:14:18 +0200

    update data laser

--- test_18_10_2017_5000/laser_data.m
+++ test_18_10_2017_5000/laser_data.m
@@ -1,10 +1,10 @@
 function [data, plane_height] = laser_data(data)
     plane_height = 1.89;
     
-    data.('r01').('laser') = 3.068;
+    data.('r01').('laser') = 3.216;
     data.('r02').('laser') = 2.313;
-    data.('r03').('laser') = 3.755;
-    data.('r12').('laser') = 4.022;
+    data.('r03').('laser') = 3.756;
+    data.('r12').('laser') = 4.003;
     data.('r13').('laser') = 2.891;
     data.('r23').('laser') = 2.654;
 end
