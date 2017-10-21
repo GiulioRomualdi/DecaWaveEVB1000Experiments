1f24505f4e81eae8b31c5410a9308b0cfdfce3c8
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-10-21 16:24:29 +0200

    fix tabs in trilat_by_type.m

--- trilateration/trilat_by_type.m
+++ trilateration/trilat_by_type.m
@@ -1,21 +1,18 @@
 function trilat_by_type = trilat_by_type(path, anchor_position)
+s
+% storage contains trilateration results evaluated with different
+% anchor_pos_data_type 
+trilat_by_type = struct();
 
-    % storage contains trilateration results evaluated with different
-    % anchor_pos_data_type 
-    trilat_by_type = struct();
-
-    % evaluate cartesian position with decawave algorithm
-    trilat_by_type.('dw') =...
-                    perform_trilateration(path, anchor_position,...
-                                          @decawave_trilateration);
+% evaluate cartesian position with decawave algorithm
+trilat_by_type.('dw') = perform_trilateration(path, anchor_position,...
+                                              @decawave_trilateration);
     
-    % evaluate cartesian position with decawave algorithm
-    trilat_by_type.('algebraic') =...
-                    perform_trilateration(path, anchor_position,...
-                                          @algebraic_trilateration);
+% evaluate cartesian position with decawave algorithm
+trilat_by_type.('algebraic') = perform_trilateration(path, anchor_position,...
+                                                @algebraic_trilateration);
                      
-    % evaluate cartesian position with decawave algorithm
-    trilat_by_type.('dw_cycle') =...
-                    perform_trilateration(path, anchor_position,...
+% evaluate cartesian position with decawave algorithm
+trilat_by_type.('dw_cycle') = perform_trilateration(path, anchor_position,...
                                           @decawave_cycle_trilateration);
 end       
