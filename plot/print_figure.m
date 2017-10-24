4c7eaa8704788f2e76c26ac07faa2aa61498ee0e
Giulio Romualdi <giulio.romualdi@gmail.com>
2017-10-24 18:45:49 +0200

    -remove export_fig submodule -remove print_figure.m

--- plot/print_figure.m
+++
@@ -1,7 +0,0 @@
-function print_figure(filename)
-% PRINT_FIGURE save a figure in a pdf file
-%   PRINT_FIGURE(filename) save the current figure in a pdf file
-
-% if a subplot figure want to be save use png extension instead of pdf
-export_fig(strcat('figures/',filename,'.pdf'), '-transparent');
-end
